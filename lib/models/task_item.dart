2edc012691020f8dafc08ac9dd98dfbf78dd57a3
garsonj <110767551+garsonj@users.noreply.github.com>
2025-11-13 15:54:20 +0300

    implemented  fromJson(Map<String, dynamic> json) factory constructor

--- lib/models/task_item.dart
+++ lib/models/task_item.dart
@@ -23,5 +23,31 @@ class TaskItem {
     };
   }
 
- 
+  factory TaskItem.fromJson(Map<String, dynamic> json) {
+    return TaskItem(
+      id: json['id'] is int ? json['id'] as int : int.tryParse('${json['id']}'),
+      title: json['title'] as String? ?? '',
+      priority: (json['priority'] as num?)?.toInt() ?? 1,
+      description: json['description'] as String? ?? '',
+      isCompleted: (json['isCompleted'] is int
+              ? (json['isCompleted'] as int) == 1
+              : (json['isCompleted'] == true)),
+    );
+  }
+
+  TaskItem copyWith({
+    int? id,
+    String? title,
+    int? priority,
+    String? description,
+    bool? isCompleted,
+  }) {
+    return TaskItem(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      priority: priority ?? this.priority,
+      description: description ?? this.description,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
 }
