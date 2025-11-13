c0b96a2f8aa2e515f080190f1b39d6f3abfaf1da
garsonj <110767551+garsonj@users.noreply.github.com>
2025-11-13 15:59:15 +0300

    created methods for initializing db and creating a db

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/task_item.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+  DatabaseHelper._init();
+
+  static const String _tableName = 'tasks';
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('task_notes_manager.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    // Use appropriate directory based on platform
+    final Directory appDir;
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      appDir = await getApplicationSupportDirectory();
+    } else {
+      appDir = await getApplicationDocumentsDirectory();
+    }
+    
+    final path = join(appDir.path, fileName);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $_tableName (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        priority INTEGER NOT NULL,
+        description TEXT,
+        isCompleted INTEGER NOT NULL
+      )
+    ''');
+  }
+}
